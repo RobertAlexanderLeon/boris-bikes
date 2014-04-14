b99822c0a471f21267ad62eeec629a6f687e82a6
Robert Leon <llexi@hotmail.com>
2014-04-14 18:16:45 +0100

    4 tests passed for person spec

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,23 @@
 class Person
 
-	def has_bike?
+	def initialize
+		@bike
 	end
+
+	def has_bike?
+        !@bike.nil? 	
+    end
+
+	def rent_bike_from(station)
+		@bike = station.release_bike
+	end
+
+	def fall_off(bike)
+		@bike = bike.break!
+	end
+
+
+
+
 
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -4,12 +4,30 @@ describe Person do
 
 	let(:person) { Person.new }
 
-	it "doesn't have a back" do
+	it "doesn't have a bike" do
 		expect(person).not_to have_bike
 	end
 
 	it "can rent a bike from the docking station" do
+		station = double :station
+		expect(station).to receive(:release_bike)
+		person.rent_bike_from(station)
 	end
-	
+
+	it "gets a bike when it rent_bike_from station" do
+	    station = double :station, release_bike: :bike
+	    person.rent_bike_from(station)
+	    expect(person).to have_bike
+	end
+
+	it "can fall off the bike" do
+		bike = double :bike
+		expect(bike).to receive(:break!)
+		
+		person = Person.new
+		person.fall_off(bike)
+	end
+
+
 
 end
