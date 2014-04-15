bc5e869a03ecfc4e16848aa9417b0af66b0b865c
Robert Leon <llexi@hotmail.com>
2014-04-15 12:33:27 +0100

    Outlined tests for Docking station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,4 +8,19 @@ describe Docking_Station do
 	expect(station).to have_bikes
 	end
 
+	it 'can release bikes' do
+	end
+
+	it 'can accept bikes' do
+	end
+
+	it 'has a limited capacity' do
+	end
+
+	it 'knows if a bike is broken' do
+	end
+
+	it 'knows how many bikes are docked' do
+	end
+
 end
